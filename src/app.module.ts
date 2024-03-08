25fe6457904037e620d4ee3056f0dc21799d4b14
nicoheredia59 <nicoheredia59@gmail.com>
2024-03-08 10:52:10 -0300

    refactor: update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { EnvironmentModule } from '@/environment/environment.module';
+import { ResendModule } from '@/resend/resend.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EnvironmentModule, ResendModule],
 })
 export class AppModule {}
